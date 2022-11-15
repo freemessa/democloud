f37ffdcc18860c94c95bbec36c2a54616578d022
freemessa <1480391673@qq.com>
2022-11-15 15:41:20 +0800

    add swagger config

--- clouddemo/fegin/src/main/java/com/example/fegin/controller/RegisterController.java
+++ clouddemo/fegin/src/main/java/com/example/fegin/controller/RegisterController.java
@@ -2,11 +2,16 @@ package com.example.fegin.controller;
 
 import com.example.fegin.feign.RegisterFegin;
 import com.example.fegin.service.RegisterService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/register")
+@Api(tags="RegisterApi")
 public class RegisterController {
     @Autowired
     private RegisterFegin registerFegin;
@@ -15,13 +20,24 @@ public class RegisterController {
     private RegisterService registerService;
 
 
-
+    /**
+     * register
+     *
+     * @return string
+     **/
+    @ApiOperation("注册")
     @GetMapping("/reg")
     public String register() {
         return registerFegin.reg();
 
     }
 
+    /**
+     * register
+     *
+     * @return string
+     **/
+    @ApiOperation("注册2")
     @GetMapping("/regb")
     public String registerB() {
         return registerService.reg();
